f305f8b210e6098365e32488e43d300d50f9985b
ryoback <back@ryoasset.com.br>
2026-09-11 17:30:51 -0300

    Update password to Xandao2019ryo!

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def check_password():
     """Retorna True se o usuário estiver autenticado com a senha correta."""
     # Obtém a senha configurada nos Secrets do Streamlit ou usa a senha padrão
-    senha_correta = st.secrets.get("PASSWORD", "xandao")
+    senha_correta = st.secrets.get("PASSWORD", "Xandao2019ryo!")
 
     if "authenticated" not in st.session_state:
         st.session_state["authenticated"] = False
